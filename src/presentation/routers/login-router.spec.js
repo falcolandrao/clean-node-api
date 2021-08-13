284927e8eb7c87a998ee62b4c03121f2474c9c31
Leandro de Azevedo <ti.leandroazevedo@gmail.com>
2021-08-12 21:13:40 -0300

    LoginRouter retorna 500 se httpRequest e httpResponse for inválido

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,11 @@
 class LoginRouter{
     route (httpRequest){
+        if (!httpRequest || !httpResponse){
+            return {
+                statusCode: 500
+            }
+        }
+        const {email, password} = httpRequest.body
         if(httpRequest.body.email || httpRequest.body.password){
             return{
                 statusCode: 400
@@ -30,6 +36,20 @@ describe('Login Router', () => {
         }
         const httpResponse = sut.route(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
+    })
+
+    test('Should return 500 if no httpRequest is provided', () =>{
+        const sut = new loginRouter()
+
+        const httpResponse = sut.route()
+        expect(httpResponse.statusCode).toBe(500)
+    })
+
+    test('Should return 500 if httpRequest has no body', () =>{
+        const sut = new loginRouter()
+        const httpRequest = {}
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(500)
     })
 
 })
